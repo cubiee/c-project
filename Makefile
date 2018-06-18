ebccac4695bdd30c2537df67ed54b13257646e9f
Joshua Harig <j.harig95@hotmail.de>
2018-06-18 17:17:35 +0200

    init

---
+++ Makefile
@@ -0,0 +1,6 @@
+CC = gcc
+LIBDIR = ./Header
+VPATH = $(LIBDIR)
+CFLAGS = -Wall -pedantic -std=c99 -c -I $(LIBDIR)
+OBJECTS = main.o PROJECT.o
+PROJECTNAME = PROJECTNAME 
---
+++ main.c
@@ -0,0 +1,21 @@
+/*PROJECTNAME
+BESCHREIBUNG
+Autor: NAME, VORNAME
+Matr.Nr.: MATRIKELNUMMER
+Datum: DATUM
+Compile: Makefile
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include <PROJECT.h>
+
+int main(int argc, char *argv[]){
+	/*
+
+	CODE
+	
+	*/
+	return 0;
+}
